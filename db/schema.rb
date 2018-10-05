83d43cb05b110b9dfbcaacfd37c35991e8fc433c
Ev1lxg3n1us <evilx.emelyanov@face-team.com>
2018-10-05 15:09:23 +0300

    admin-config

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_05_071907) do
+ActiveRecord::Schema.define(version: 2018_10_05_120748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,37 @@ ActiveRecord::Schema.define(version: 2018_10_05_071907) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "count", default: 0
+    t.string "username", default: "auto generate", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "images_id"
+    t.bigint "users_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["images_id"], name: "index_comments_on_images_id"
+    t.index ["users_id"], name: "index_comments_on_users_id"
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "full_size_path", null: false
+    t.string "medium_size_path", null: false
+    t.string "small_size_path", null: false
+    t.float "size", null: false
+    t.bigint "users_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_images_on_category_id"
+    t.index ["users_id"], name: "index_images_on_users_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -53,4 +84,8 @@ ActiveRecord::Schema.define(version: 2018_10_05_071907) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "images", column: "images_id"
+  add_foreign_key "comments", "users", column: "users_id"
+  add_foreign_key "images", "categories"
+  add_foreign_key "images", "users", column: "users_id"
 end
