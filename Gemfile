101b37bfe2a9314afe04a79eb47d2c78cf73b1ef
Ev1lxg3n1us <evilx.emelyanov@face-team.com>
2018-10-11 10:37:59 +0300

    v0.5.1

--- Gemfile
+++ Gemfile
@@ -23,6 +23,9 @@ gem 'pundit'
 
 gem 'carrierwave', '~> 1.0'
 
+# monitoring users action
+gem 'paper_trail'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 # Use postgresql as the database for Active Record
--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,12 +3,34 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
+
+
+    panel "Statistics" do
+      table_for Category.order("created_at DESC").limit(5) do |category|
+        category.column(:name)
+        category.column(:count)
+      end
+
+      table_for Comment.order("created_at DESC").limit(5) do |comment|
+        comment.column(:body)
+        comment.column(:user_id)
+        comment.column(:image_id)
+      end
+
+      table_for Image.order("created_at DESC").limit(10) do |image|
+        image.column(:path)
+        image.column(:user_id)
+        image.column(:category_id)
       end
     end
+
+
 
     # Here is an example of a simple dashboard with columns and panels.
     #
