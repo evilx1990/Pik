af6e7739521131d61d2c0f1ab4156048f05dfae4
Ev1lxg3n1us <evilx.emelyanov@face-team.com>
2018-12-28 13:25:48 +0200

    add https:// to default_url_options for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'gentle-atoll-34657.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://gentle-atoll-34657.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
