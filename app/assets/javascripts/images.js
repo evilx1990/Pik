d1f97311b4f101c320aecee37b4d318da9a0e9f9
Ev1lxg3n1us <evilx.emelyanov@face-team.com>
2019-01-18 14:24:42 +0200

    add key control to slider images/index

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -2,34 +2,49 @@ let current_index;  // Индекс текущего изображения
 let modal;          // Модальное окно
 let images;         // Массив картинок в модальном окне
 
+window.onkeydown = function (event) {
+  if(!['ArrowRight', 'ArrowLeft'].includes(event.key)){
+    return null;
+  }
+
+  if(modal.style.display === 'block') {
+    event.key === 'ArrowRight'? next() : prev();
+  }
+};
+
 function showModal(index) {
   modal = document.querySelector('#index-slider');
-  images = document.querySelectorAll('.slider-image');
+  images = document.querySelectorAll('.hidden-image');
 
   modal.style.display = 'block';
   modal.parentNode.style.overflow = 'hidden';
 
+  images[index].classList.remove('hidden-image');
   images[index].classList.add('show-image');
   current_index = index;
 }
 
 function next() {
+  images[current_index].classList.add('hidden-image');
   images[current_index++].classList.remove('show-image');
 
   if (current_index === images.length) {
     current_index = 0;
   }
 
+  images[current_index].classList.remove('hidden-image');
   images[current_index].classList.add('show-image');
 }
 
 function prev() {
+  images[current_index].classList.add('hidden-image');
   images[current_index--].classList.remove('show-image');
 
   if (current_index < 0) {
     current_index = images.length - 1;
   }
 
+  images[current_index].classList.remove('hidden-image');
   images[current_index].classList.add('show-image');
 }
 
